c91802950fa1b135f1ea575fb2589afa669223f0
Lucca Erick <bernardesluccaerickthomasberna@gmail.com>
2025-12-28 00:47:41 -0300

    ✨ Implements test success validator

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTest.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = new RequestRegisterExpenseJson
+            {
+                Title = "Title",
+                Description = "Description",
+                Amount = 100,
+                Payment = PaymentType.DebitCard,
+                Date = DateTime.Now.AddDays(-1),
+            };
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
