e5ef174e1b2171a96882a2fbc5e0cc3c31c390ce
Lucca Erick <bernardesluccaerickthomasberna@gmail.com>
2025-12-28 21:38:07 -0300

    ✨ Created connection with database mysql

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        // Variavel que dara acesso a tabela expenses
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root_password_123";
+
+            var serverVersion = new MySqlServerVersion(new Version(8,0));
+
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
+}
